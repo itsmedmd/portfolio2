4015bd43c7b4f0ee20664c4c381f9ab426c891b8
ElqBell <silenc8@gmail.com>
2021-03-08 14:29:02 +0200

    2 styling navigation and making it visually accessible

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -7,7 +7,7 @@ export const Navigation = () => {
     <nav className="nav">
         <ul className="nav__list">
             <li className="nav__item">
-                <Link className="nav__link" activeClassName="nav__link--active" to="/">
+                <Link className="nav__link nav__link--home" activeClassName="nav__link--active" to="/">
                     <span className="nav__big-text">DB</span>
                     <span className="nav__small-text">Home</span>
                 </Link>
