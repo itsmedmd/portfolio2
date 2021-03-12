d875fd317cce03f26c8d25eb200d584813fb15e2
ElqBell <silenc8@gmail.com>
2021-03-13 00:30:41 +0200

    3 adding background images and fixing routing links

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,9 @@ const HomePage = () => {
   return (
     <Layout className="homepage">
       <h1 className="homepage__title">
-        Your friendly neighbourhood spider
+        Your friendly neighbourhood spider with
         <br/>
-        with a passion for webs
+        a passion for webs
       </h1>
       <div className="homepage__action">
         <Link className="button-link" to="/about">About</Link>
