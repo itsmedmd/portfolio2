8ce0763273064f369c041389a5d56443ac60f788
ElqBell <silenc8@gmail.com>
2021-03-15 15:51:34 +0200

    3 fixing button alignment and homepage margins

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,7 @@ const HomePage = () => {
   return (
     <Layout className="homepage">
       <h1 className="homepage__title">
-        Your friendly neighbourhood spider with
-        <br/>
-        a passion for webs
+        Your friendly neighbourhood spider with a passion for webs
       </h1>
       <div className="homepage__action">
         <Link className="button-link" to="/about">About</Link>
