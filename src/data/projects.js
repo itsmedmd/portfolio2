45657853f8ab76e48a81d37095c241273ae15740
ElqBell <silenc8@gmail.com>
2021-04-24 20:31:24 +0300

    8 removing placeholder project data

--- src/data/projects.js
+++ src/data/projects.js
@@ -82,27 +82,6 @@ const projectsData = [
             "SCSS",
             "HTML"
         ]
-    },
-    {
-        title: "Placeholder text",
-        description: "A photography and videography portfolio website.",
-        img: "project5.jpg",
-        features: [
-            "Image and video lazyloading",
-            "Automatic image compression",
-            "Automatic image clean-up(deletion) from Amazon S3",
-            "Automatic 'videos' page HTML code generation",
-            "Automatic HTML code generation of individual image galleries' pages and 'all-galleries' page",
-            "Email contact form"
-        ],
-        tools: [
-            "Amazon Web Services(AWS)",
-            "NodeJS with AWS Lambda",
-            "NodeJS image processing module 'sharp'",
-            "JavaScript",
-            "SCSS",
-            "HTML"
-        ]
     }
 ];
 
