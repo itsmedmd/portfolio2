9b043b6c3d7ac0913dc20f1a52dfae97c992987c
ElqBell <silenc8@gmail.com>
2021-05-22 19:00:41 +0300

    23 adding fully styled and finished page load animation

--- src/components/pageLoadAnimation/PageLoadAnimation.jsx
+++ src/components/pageLoadAnimation/PageLoadAnimation.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./pageLoadAnimation.scss";
 
-export const PageLoadAnimation = ({ text }) => {
+export const PageLoadAnimation = ({ text, sendIsAnimationActiveSignal }) => {
+  const [isActive, setIsActive] = useState(true);
+  const [isFinished, setIsFinished] = useState(false);
+
+  useEffect(() => {
+    const handleAnimationFinish = () => {
+      sendIsAnimationActiveSignal(false);
+      setIsFinished(true);
+    }; 
+
+    const timeout1 = setTimeout(() => setIsActive(false), 1500);
+    const timeout2 = setTimeout(handleAnimationFinish, 2000);
+
+    return () => {
+      clearTimeout(timeout1);
+      clearTimeout(timeout2);
+    };
+  }, [sendIsAnimationActiveSignal]);
+
   return (
-      <div className="page-load-animation">
+      <div
+        className={`
+          page-load-animation
+          ${isActive ? "" : "page-load-animation--invisible"}
+          ${isFinished ? "page-load-animation--finished" : ""}
+        `}
+      >
         <h2 className="page-load-animation__text">
           {
             text.split(" ")
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "gatsby";
 import { Layout, PageLoadAnimation } from "components";
 import "./index.scss";
 
 const HomePage = () => {
+  const [isAnimationActive, setIsAnimationActive] = useState(true);
+
+  const handleAnimationChange = (val) => {
+    setIsAnimationActive(val);
+  };
+
+  console.log("status:", isAnimationActive);
+
   return (
-    <Layout className="homepage" animation={true} >
-      <PageLoadAnimation text="Deimantas Butėnas" />
+    <Layout className="homepage" animation={isAnimationActive} >
+      <PageLoadAnimation text="Deimantas Butėnas" sendIsAnimationActiveSignal={handleAnimationChange} />
       <h1 className="homepage__title">
         Your friendly neighbourhood spider with a passion for webs
       </h1>
