81cddb7269405c6ac3cf2e8162ee805d92eb7b7c
ElqBell <silenc8@gmail.com>
2021-06-06 14:51:10 +0300

    11 making all pages except 'projects' page responsive from 768px and above

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -79,30 +79,33 @@ const Contact = ({ data }) => {
 
       <article className="contact__content">
         <section className="contact__links">
+          <h2 className="contact__links-title">You can reach me at:</h2>
           <a
-            href="mailto:deimantas.butenas@gmail.com"
-            className="contact__email-link"
-          >
-            deimantas.butenas@gmail.com
-          </a>
-          <a
-            className="button-link button-link--medium"
+            className="button-link button-link--no-margin button-link--medium"
             href="https://www.linkedin.com/in/deimantas-but%C4%97nas-85870a192/"
           >
             <img className="button-link__image" src={images.linkedin} alt="" />
             LinkedIn
           </a>
           <a
-            className="button-link button-link--medium"
+            className="button-link button-link--no-margin button-link--medium"
             href="https://github.com/ElqBell/"
           >
             <img className="button-link__image" src={images.github} alt="" />
             GitHub
           </a>
+          <a
+            href="mailto:deimantas.butenas@gmail.com"
+            className="contact__email-link"
+          >
+            deimantas.butenas@gmail.com
+          </a>
         </section>
 
         <aside className="contact__message">
-          <h2 className="contact__message-title">Send me a quick message.</h2>
+          <h2 className="contact__message-title">
+            Or send me a quick message:
+          </h2>
 
           <form className="contact__form" onSubmit={handleSubmit}>
             <FormLoader
