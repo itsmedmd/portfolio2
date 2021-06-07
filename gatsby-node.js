2122f85a78c0d97aca5354314612da079cb709de
ElqBell <silenc8@gmail.com>
2021-06-07 19:50:18 +0300

    11 splitting about page description into two paragraphs

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,116 +1,116 @@
 const path = require("path");
 const projectsData = require("./src/data/projects");
 const aboutData = require("./src/data/about");
-const createSVGImagesObject = require("./src/utils/createSVGImagesObject").createSVGImagesObject;
+const createSVGImagesObject =
+  require("./src/utils/createSVGImagesObject").createSVGImagesObject;
 
 // custom webpack config
 exports.onCreateWebpackConfig = ({ actions }) => {
-    actions.setWebpackConfig({
-        resolve: {
-            alias: {
-                components: path.resolve(__dirname, "src/components"),
-                images: path.resolve(__dirname, "src/images"),
-                data: path.resolve(__dirname, "src/data"),
-                utils: path.resolve(__dirname, "src/utils"),
-                styles: path.resolve(__dirname, "src/styles"),
-            },
-        },
-    });
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, "src/components"),
+        images: path.resolve(__dirname, "src/images"),
+        data: path.resolve(__dirname, "src/data"),
+        utils: path.resolve(__dirname, "src/utils"),
+        styles: path.resolve(__dirname, "src/styles"),
+      },
+    },
+  });
 };
 
 // create graphql nodes from data files for build time data
 exports.sourceNodes = async ({
-    actions: { createNode },
-    createNodeId,
-    createContentDigest,
+  actions: { createNode },
+  createNodeId,
+  createContentDigest,
 }) => {
-    // "project" pages' data
-    projectsData.map(project => {
-        // create slug (url) of the project page
-        const formattedTitle = project.title.toLowerCase().replace(/[\s-]+/g, "-");
-        const slug = `projects/${formattedTitle}`;
+  // "project" pages' data
+  projectsData.map((project) => {
+    // create slug (url) of the project page
+    const formattedTitle = project.title.toLowerCase().replace(/[\s-]+/g, "-");
+    const slug = `projects/${formattedTitle}`;
 
-        createNode({
-            ...project,
-            id: createNodeId(project.title),
-            slug,
-            parent: null,
-            children: [],
-            internal: {
-                type: `Project`,
-                contentDigest: createContentDigest(project),
-            }
-        })}
-    );
-
-    // "about" page data
     createNode({
-        ...aboutData,
-        id: createNodeId(aboutData.description),
-        parent: null,
-        children: [],
-        internal: {
-            type: `About`,
-            contentDigest: createContentDigest(aboutData),
-        }
+      ...project,
+      id: createNodeId(project.title),
+      slug,
+      parent: null,
+      children: [],
+      internal: {
+        type: `Project`,
+        contentDigest: createContentDigest(project),
+      },
     });
+  });
+
+  // "about" page data
+  createNode({
+    ...aboutData,
+    id: createNodeId(aboutData.description[0]),
+    parent: null,
+    children: [],
+    internal: {
+      type: `About`,
+      contentDigest: createContentDigest(aboutData),
+    },
+  });
 };
 
 //programatically create "Project" pages
 exports.createPages = async ({ graphql, actions }) => {
-    const { createPage } = actions;
-    const projectsQuery = await graphql(`
-        query ProjectsQuery {
-            allFile(filter: {extension: {regex: "/^((?!svg).)*$/"}}) {
-                nodes {
-                  childImageSharp {
-                    gatsbyImageData(
-                        placeholder: DOMINANT_COLOR
-                        quality: 75
-                    )
-                  }
-                  relativePath
-                }
-            }
-            svgAllFile: allFile(filter: {extension: {regex: "/svg/"}}) {
-                nodes {
-                  relativePath
-                  publicURL
-                }
-            }
-            allProject {
-                nodes {
-                    description
-                    features
-                    img
-                    title
-                    tools {
-                        text
-                        img
-                    }
-                    slug
-                }
-            }
+  const { createPage } = actions;
+  const projectsQuery = await graphql(`
+    query ProjectsQuery {
+      allFile(filter: { extension: { regex: "/^((?!svg).)*$/" } }) {
+        nodes {
+          childImageSharp {
+            gatsbyImageData(placeholder: DOMINANT_COLOR, quality: 75)
+          }
+          relativePath
         }
-    `);
+      }
+      svgAllFile: allFile(filter: { extension: { regex: "/svg/" } }) {
+        nodes {
+          relativePath
+          publicURL
+        }
+      }
+      allProject {
+        nodes {
+          description
+          features
+          img
+          title
+          tools {
+            text
+            img
+          }
+          slug
+        }
+      }
+    }
+  `);
 
-    const { allFile, allProject, svgAllFile } = projectsQuery.data;
-    const SVGImages = createSVGImagesObject(svgAllFile.nodes);
+  const { allFile, allProject, svgAllFile } = projectsQuery.data;
+  const SVGImages = createSVGImagesObject(svgAllFile.nodes);
 
-    allProject.nodes.forEach(node => {
-        const {img, slug, ...data} = node;
-        const sharpImg = allFile.nodes.find(imgSharp => imgSharp.relativePath === img);
-        createPage({
-            path: slug,
-            component: path.resolve(`src/templates/project/Project.jsx`),
-            context: {
-                // Data passed to context is available
-                // in page queries as GraphQL variables.
-                ...data,
-                SVGImages,
-                sharpImg: sharpImg.childImageSharp,
-                slug
-            }
-        });
+  allProject.nodes.forEach((node) => {
+    const { img, slug, ...data } = node;
+    const sharpImg = allFile.nodes.find(
+      (imgSharp) => imgSharp.relativePath === img
+    );
+    createPage({
+      path: slug,
+      component: path.resolve(`src/templates/project/Project.jsx`),
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables.
+        ...data,
+        SVGImages,
+        sharpImg: sharpImg.childImageSharp,
+        slug,
+      },
     });
+  });
 };
--- src/data/about.js
+++ src/data/about.js
@@ -1,6 +1,8 @@
 const aboutData = {
-  description:
-    "I don't know who you are. I don't know what you want. If you are looking for a back-end developer, I can tell you I don't have the expertise, but what I do have are a very particular set of skills. Skills I have acquired over many cold and sleepless nights. Skills that are necessary in times like these. If you exit this website now that'll be the end of it. I will not look for you, I will not pursue you, but if you decide to proceed, I will wait for your message, I will reply to it and I will write the best front-end code you've seen.",
+  description: [
+    "I don't know who you are. I don't know what you want. If you are looking for a back-end developer, I can tell you I don't have the expertise, but what I do have are a very particular set of skills. Skills I have acquired over many cold and sleepless nights. Skills that are necessary in times like these.",
+    "If you exit this website now that'll be the end of it. I will not look for you, I will not pursue you, but if you decide to proceed, I will wait for your message, I will reply to it and I will write the best front-end code you've seen.",
+  ],
   experiences: [
     "Working on projects in Agile teams",
     "Developing accessible websites",
