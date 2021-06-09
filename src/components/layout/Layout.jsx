2af0ee904769d93ad28de2d11f6af47951faa554
ElqBell <silenc8@gmail.com>
2021-06-09 15:42:28 +0300

    18 adding metadata

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -28,9 +28,45 @@ export const Layout = ({
       `}
     >
       <Helmet>
+        <meta name="title" content="Front-end developer Deimantas Butėnas" />
         <meta
           name="description"
           content="Portfolio website of a front-end developer Deimantas Butėnas."
+        />
+        <meta
+          name="keywords"
+          content="deimantas butėnas, deimantas butenas, web development, frontend, front-end, front end, portfolio, design, web design, react, gatsby, javascript, html, css, scss, sass, git, aws, graphql"
+        />
+        <link rel="canonical" href="https://www.deimantasb.com/" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.deimantasb.com/" />
+        <meta
+          property="og:title"
+          content="Front-end developer Deimantas Butėnas"
+        />
+        <meta
+          property="og:description"
+          content="Portfolio website of a front-end developer Deimantas Butėnas where you can see his work!"
+        />
+        <meta
+          property="og:image"
+          content="https://www.deimantasb.com/meta-og-image.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.deimantasb.com/" />
+        <meta
+          property="twitter:title"
+          content="Frontend developer Deimantas Butėnas"
+        />
+        <meta
+          property="twitter:description"
+          content="Portfolio website of a front-end developer Deimantas Butėnas where you can see his work!"
+        />
+        <meta
+          property="twitter:image"
+          content="https://www.deimantasb.com/meta-og-image.png"
         />
       </Helmet>
 
